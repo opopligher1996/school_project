f1b79a79798c479ccfcd425902f713f0d6bcce22
opopligher1996 <opopligher1996@gmail.com>
2017-12-20 22:57:08 +0800

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+<link href="assets/css/style_profile.css" rel="stylesheet">        
+<script type="text/javascript" src="https://cdn.firebase.com/js/client/2.2.3/firebase.js"></script>
+<script src="http://www.gstatic.com/firebasejs/live/3.1/firebase.js"> </script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+</head>
+
+<body background="assets/images/ust.jpg">
+
+<div class="container" align=center >
+  <div class="row">
+    <div class="span6 offset3">
+      <div class="profile">
+         
+         
+        <div class="iconset" align="left">
+         <button type="reset" style="background-color:transparent; border-color:transparent;"> 
+  		 	<img src="assets/button/add.png" height="30"/>
+		 </button>
+         <button type="reset" style="background-color:transparent; border-color:transparent;"> 
+  		 	<img src="assets/button/add.png" height="30"/>
+		 </button>
+         <button type="reset" style="background-color:transparent; border-color:transparent;"> 
+  		 	<img src="assets/button/add.png" height="30"/>
+		 </button>
+                  <button type="reset" style="background-color:transparent; border-color:transparent;"> 
+  		 	<img src="assets/button/add.png" height="30"/>
+		 </button>
+        </div>
+        <hr />
+        <div class="user-info">
+          <div class="profile-picture" style="background:"" center center"/>
+        </div>
+        <div  class="name">
+          <span id="name"></span>
+        </div>
+        <div class="description">
+          <span id="des"></span>
+          <pre id="object">
+          </pre>
+          <ul id="list">
+          </ul>
+
+        </div>
+        <div class="iconset big">
+          <div class="set">
+            <i class="icon-pencil"></i>
+            <span class="text">icon1</span>
+          </div>
+          <div class="set">
+            <i class="icon-user"></i>
+            <span class="text">icon2</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<script src="assets/js/app.js"></script>
+</body>
+</html>
